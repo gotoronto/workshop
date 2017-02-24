8a2908f948270cf25f02d71f6abd1c39e1d1ecb1
Tim Anema <tim.anema@shopify.com>
2017-02-24 11:12:45 -0500

    Added form handling and REST JSON examples

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Go Toronto Workshop
 
-This is a workshop to teach new people to Go
+This is a workshop to teach new people Go
+
+## Slides
+
+`workshop.slides` is the presentation for this workshop. You will need to install
+the go present tool to display it.
+
+To install the present tool, use go get: `go get golang.org/x/tools/cmd/present`
+
+## Code
+
+If you follow along with the workshop you will be able to use the code without
+installing any dependancies.
--- forms/main.go
+++ forms/main.go
@@ -1,89 +1,18 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
-	"strconv"
 
 	"github.com/pressly/chi"
-	"github.com/pressly/chi/render"
+
+	"github.com/gotoronto/workshop/forms/controllers/todos"
 )
 
-type Todo struct {
-	Task string
-	Done bool
-}
-
-func (todo *Todo) Validate() error {
-	return nil
-}
-
 func main() {
-	indexTmpl := template.Must(template.ParseFiles("views/todos/index.html"))
-	showTmpl := template.Must(template.ParseFiles("views/todos/show.html"))
-	formTmpl := template.Must(template.ParseFiles("views/todos/form.html"))
-
-	todos := []Todo{
-		{"Learn Go", true},
-		{"Learn Go Web", true},
-		{"Create a web app in Go", false},
-	}
-
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		indexTmpl.Execute(w, struct{ Todos []Todo }{todos})
+		http.Redirect(w, r, "/todos", 302)
 	})
-	r.Get("/new", func(w http.ResponseWriter, r *http.Request) {
-		formTmpl.Execute(w, struct {
-			Error error
-			Task  Todo
-		}{nil, Todo{}})
-	})
-	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		indexTmpl.Execute(w, struct{ Todos []Todo }{todos})
-
-		done, _ := strconv.ParseBool(r.FormValue("done"))
-		todo := Todo{
-			Task: r.FormValue("task"),
-			Done: done,
-		}
-
-		err := todo.Validate()
-		if err != nil {
-			formTmpl.Execute(w, struct {
-				Error error
-				Task  Todo
-			}{err, todo})
-			return
-		}
-
-		http.Redirect(w, r, r.RequestURI+"/", 200)
-	})
-	r.Get("/:id", func(w http.ResponseWriter, r *http.Request) {
-		idStr := chi.URLParam(r, "id")
-		id, err := strconv.Atoi(idStr)
-		if err != nil {
-			render.PlainText(w, r, "Invalid Todo ID")
-		} else if id < 0 || id >= len(todos) {
-			render.PlainText(w, r, "404 Todo not found")
-		} else {
-			showTmpl.Execute(w, todos[id])
-		}
-	})
-	r.Get("/:id/edit", func(w http.ResponseWriter, r *http.Request) {
-		idStr := chi.URLParam(r, "id")
-		id, err := strconv.Atoi(idStr)
-		if err != nil {
-			render.PlainText(w, r, "Invalid Todo ID")
-		} else if id < 0 || id >= len(todos) {
-			render.PlainText(w, r, "404 Todo not found")
-		} else {
-			formTmpl.Execute(w, struct {
-				Error error
-				Task  Todo
-			}{nil, todos[id]})
-		}
-	})
-
+	r.Mount("/todos", todos.Routes())
 	http.ListenAndServe(":8080", r)
 }
