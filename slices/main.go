a837c0c1bc8570dab3df14ef022df11a66096fde
Tim Anema <tim.anema@shopify.com>
2017-03-14 17:52:46 -0400

    took out solution

--- slices/main.go
+++ slices/main.go
@@ -4,14 +4,6 @@ import "github.com/gotoronto/workshop/slices/display"
 
 func Pic(dx, dy int) [][]int {
 	// implement
-	output := make([][]int, dy)
-	for y := range output {
-		output[y] = make([]int, dx)
-		for x := range output[y] {
-			output[y][x] = y
-		}
-	}
-	return output
 }
 
 func main() {
